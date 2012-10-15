6b6cf70fcef90a208aaace5b2847004bab612311
Wander <lywander@gmail.com>
2012-10-16 06:10:35 +0800

    screen1 almost done

---
+++ SchemaBuilderPrototyping/SchemaBuilderPrototyping/SchemaBuilderPrototypingScreens/Screen_1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SchemaBuilderPrototypingScreens
+{
+	public partial class Screen_1 : UserControl
+	{
+		public Screen_1()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+
+		private void TextBox_GotFocus(object sender, System.Windows.RoutedEventArgs e)
+		{
+			(sender as TextBox).SelectAll();
+			customAgeRadioButton.IsChecked = true;
+		}
+	}
+}
